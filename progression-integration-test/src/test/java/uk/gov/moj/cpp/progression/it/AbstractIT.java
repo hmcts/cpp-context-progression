d65d71b2c87f26fce2de51e4d4f19e8559a0d345
hshaik <hasan.shaik@ext.soprasteria.com>
2016-06-03 16:13:14 +0100

    INTEGRATION_HOST_KEY for IT test execution
    Change-Id: Ia26e22d1049b2a5defb5368f1b1c5f213b7193ee

--- progression-integration-test/src/test/java/uk/gov/moj/cpp/progression/it/AbstractIT.java
+++ progression-integration-test/src/test/java/uk/gov/moj/cpp/progression/it/AbstractIT.java
@@ -1,40 +1,40 @@
 package uk.gov.moj.cpp.progression.it;
 
-import com.jayway.restassured.builder.RequestSpecBuilder;
-import com.jayway.restassured.specification.RequestSpecification;
-import org.apache.commons.lang3.StringUtils;
-
 import java.io.IOException;
 import java.io.InputStream;
 import java.util.Properties;
 
+import org.apache.commons.lang3.StringUtils;
+
+import com.jayway.restassured.builder.RequestSpecBuilder;
+import com.jayway.restassured.specification.RequestSpecification;
+
 public class AbstractIT {
 
-    protected Properties prop;
-    protected RequestSpecification reqSpec;
-    protected String baseUri;
+	protected Properties prop;
+	protected RequestSpecification reqSpec;
+	protected String baseUri;
 
-  	public AbstractIT() {
-        readConfig();
-        setRequestSpecification();
-    }
+	public AbstractIT() {
+		readConfig();
+		setRequestSpecification();
+	}
 
-    private void readConfig() {
-        prop = new Properties();
-        ClassLoader loader = Thread.currentThread().getContextClassLoader();
-        InputStream stream = loader.getResourceAsStream("endpoint.properties");
-        try {
-            prop.load(stream);
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-        String baseUriProp = System.getProperty("baseUri");
-        baseUri = (StringUtils.isNotEmpty(baseUriProp) ? baseUriProp : prop.getProperty("base-uri"));
-    }
+	private void readConfig() {
+		prop = new Properties();
+		ClassLoader loader = Thread.currentThread().getContextClassLoader();
+		InputStream stream = loader.getResourceAsStream("endpoint.properties");
+		try {
+			prop.load(stream);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		String baseUriProp = System.getProperty("INTEGRATION_HOST_KEY");
+		baseUri = (StringUtils.isNotEmpty(baseUriProp) ? "http://" + baseUriProp + ":8080" : prop.getProperty("base-uri"));
+	}
 
-    private void setRequestSpecification() {
-        reqSpec = new RequestSpecBuilder().setBaseUri(baseUri).build();
-    }
-    
-    
+	private void setRequestSpecification() {
+		reqSpec = new RequestSpecBuilder().setBaseUri(baseUri).build();
+	}
+
 }
