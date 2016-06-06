2353a7a6ebacf0d7b87fbff7d9c4da117288fecf
Ted Pritchard <ted.pritchard@soprasteria.com>
2016-06-06 15:37:55 +0100

    Infinity JEE Progression: Add event listener unit tests
    Change-Id: I8d001eafa3db43c213409d7cc894bb8fcf2391d6

---
+++ progression-event/progression-event-listener/src/test/java/uk/gov/moj/cpp/progression/event/listener/TrialTimeEstimatedByProsecutionEventListenerTest.java
@@ -0,0 +1,59 @@
+package uk.gov.moj.cpp.progression.event.listener;
+
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+import java.util.Optional;
+
+import javax.json.JsonObject;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.runners.MockitoJUnitRunner;
+
+import uk.gov.justice.services.common.converter.JsonObjectToObjectConverter;
+import uk.gov.justice.services.messaging.JsonEnvelope;
+import uk.gov.justice.services.messaging.Metadata;
+import uk.gov.moj.cpp.progression.domain.event.ProsecutionTrialEstimateAdded;
+import uk.gov.moj.cpp.progression.event.service.CaseService;
+
+@RunWith(MockitoJUnitRunner.class)
+public class TrialTimeEstimatedByProsecutionEventListenerTest {
+
+	@Mock
+	private ProsecutionTrialEstimateAdded prosecutionTrialEstimateAdded;
+	
+	@Mock
+	private JsonEnvelope envelope;
+    
+    @Mock
+    private JsonObject payload;
+    
+    @Mock
+    private Metadata metadata;
+    
+    @Mock
+    private JsonObjectToObjectConverter jsonObjectConverter;
+
+    @Mock
+    private CaseService service;
+
+    @InjectMocks
+    private TrialTimeEstimatedByProsecutionEventListener listener;
+
+    @Test
+    public void testProcessEvent() throws Exception {
+
+    	when(envelope.payloadAsJsonObject()).thenReturn(payload);
+    	when(envelope.metadata()).thenReturn(metadata);
+    	when(metadata.version()).thenReturn(Optional.of(0l));
+    	when(jsonObjectConverter.convert(payload, ProsecutionTrialEstimateAdded.class)).thenReturn(prosecutionTrialEstimateAdded);
+
+        listener.processEvent(envelope);
+
+        verify(service).addTrialEstimateProsecution(prosecutionTrialEstimateAdded, 0l);
+    }
+
+}
