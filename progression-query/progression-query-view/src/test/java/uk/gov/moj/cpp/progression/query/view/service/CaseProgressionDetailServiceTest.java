d51b36cd93e26096253fc09ade3a940d409c584f
jayenhc@yahoo.com <jayenhc@yahoo.com>
2016-06-30 12:05:50 +0100

    case status change from IN_REVIEW to ASSIGNED_FOR_REVIEW
    Change-Id: I57084353cdbcf68cf12c8b3aafa360c81aaa50e7

--- progression-query/progression-query-view/src/test/java/uk/gov/moj/cpp/progression/query/view/service/CaseProgressionDetailServiceTest.java
+++ progression-query/progression-query-view/src/test/java/uk/gov/moj/cpp/progression/query/view/service/CaseProgressionDetailServiceTest.java
@@ -145,9 +145,9 @@ public class CaseProgressionDetailServiceTest {
     @Test
     public void shouldReturnCasesForStatus() throws Exception {
 
-        final String status = "COMPLETED,IN_REVIEW";
+        final String status = "COMPLETED,ASSIGNED_FOR_REVIEW";
         final List<CaseStatusEnum> statusList =
-                        Arrays.asList(CaseStatusEnum.COMPLETED, CaseStatusEnum.IN_REVIEW);
+                        Arrays.asList(CaseStatusEnum.COMPLETED, CaseStatusEnum.ASSIGNED_FOR_REVIEW);
 
         when(caseProgressionDetailRepository.findByStatus(statusList)).thenReturn(Arrays.asList(
                         caseProgressionDetail1, caseProgressionDetail2, caseProgressionDetail3));
