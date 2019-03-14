6af5c3f85ec59343f02f5c9d95bae12c1f0633b7
jayenhc <jayenhc@yahoo.com>
2019-03-14 11:42:02 +0000

    GPE-8669 Fix for flaky IT tests
    Change-Id: Ifc0fdccdb968690f630542b24232d0b8391959c2

--- progression-integration-test/src/test/java/uk/gov/moj/cpp/progression/SearchCasesIT.java
+++ progression-integration-test/src/test/java/uk/gov/moj/cpp/progression/SearchCasesIT.java
@@ -1,20 +1,19 @@
 package uk.gov.moj.cpp.progression;
 
-import static junit.framework.TestCase.assertEquals;
-import static junit.framework.TestCase.assertNotNull;
-import static junit.framework.TestCase.assertTrue;
+import static com.jayway.jsonpath.matchers.JsonPathMatchers.withJsonPath;
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.hamcrest.CoreMatchers.containsString;
 import static uk.gov.moj.cpp.progression.helper.PreAndPostConditionHelper.addProsecutionCaseToCrownCourt;
 
 import static uk.gov.moj.cpp.progression.helper.PreAndPostConditionHelper.addProsecutionCaseWithUrn;
-import static uk.gov.moj.cpp.progression.helper.PreAndPostConditionHelper.getCasesForSearchCriteria;
+import static uk.gov.moj.cpp.progression.helper.PreAndPostConditionHelper.verifyCasesForSearchCriteria;
 import static uk.gov.moj.cpp.progression.helper.RestHelper.createMockEndpoints;
-import static uk.gov.moj.cpp.progression.helper.RestHelper.getJsonObject;
 
+import org.hamcrest.Matcher;
 import uk.gov.moj.cpp.progression.util.ProsecutionCaseUpdateDefendantHelper;
 
 import java.util.UUID;
 
-import javax.json.JsonObject;
 
 import org.junit.Before;
 import org.junit.Test;
@@ -44,87 +43,64 @@ public class SearchCasesIT {
 
     @Test
     public void shouldGetProsecutionCaseByCaseInsensitiveFirstName() throws Exception {
-        // given
+        // when
         addProsecutionCaseToCrownCourt(caseId, defendantId);
 
-        // when
-        final String response = getCasesForSearchCriteria("harry");
         // then
-        JsonObject prosecutioncasesJsonObject = getJsonObject(response);
-        assertNotNull(prosecutioncasesJsonObject);
-        assertTrue(prosecutioncasesJsonObject.getJsonArray("searchResults").getJsonObject(0).getString("defendantName").contains(firstName));
+        verifyCasesForSearchCriteria("harry", new Matcher[]{withJsonPath("$.searchResults[0].defendantName",containsString(firstName))});
+
     }
 
     @Test
     public void shouldGetProsecutionCaseByCaseInsensitiveLastName() throws Exception {
-        // given
+        // when
         addProsecutionCaseToCrownCourt(caseId, defendantId);
 
-        // when
-        final String response = getCasesForSearchCriteria("KANE");
         // then
-        JsonObject prosecutioncasesJsonObject = getJsonObject(response);
-        assertNotNull(prosecutioncasesJsonObject);
-        assertTrue(prosecutioncasesJsonObject.getJsonArray("searchResults").getJsonObject(0).getString("defendantName").contains("Kane"));
+        verifyCasesForSearchCriteria("KANE", new Matcher[]{withJsonPath("$.searchResults[0].defendantName",containsString("Kane"))});
+
     }
 
     @Test
     public void shouldGetProsecutionCaseByFirstName() throws Exception {
-        // given
+        // when
         addProsecutionCaseToCrownCourt(caseId, defendantId);
 
-        // when
-        final String response = getCasesForSearchCriteria(firstName);
         // then
-        final JsonObject prosecutioncasesJsonObject = getJsonObject(response);
-        assertNotNull(prosecutioncasesJsonObject);
-        assertTrue(prosecutioncasesJsonObject.getJsonArray("searchResults").getJsonObject(0).getString("defendantName").contains("Harry"));
+        verifyCasesForSearchCriteria(firstName, new Matcher[]{withJsonPath("$.searchResults[0].defendantName",containsString("Harry"))});
+
     }
 
     @Test
     public void shouldGetProsecutionCaseByMiddleName() throws Exception {
-        // given
-        addProsecutionCaseToCrownCourt(caseId, defendantId);
         // when
-        final String response = getCasesForSearchCriteria(middleName);
+        addProsecutionCaseToCrownCourt(caseId, defendantId);
         // then
-        final JsonObject prosecutioncasesJsonObject = getJsonObject(response);
-        assertNotNull(prosecutioncasesJsonObject);
-
-        assertTrue(prosecutioncasesJsonObject.getJsonArray("searchResults").getJsonObject(0).getString("defendantName").contains(middleName));
+        verifyCasesForSearchCriteria(middleName, new Matcher[]{withJsonPath("$.searchResults[0].defendantName",containsString(middleName))});
     }
 
     @Test
     public void shouldGetProsecutionCaseByLastName() throws Exception {
-        // given
-        addProsecutionCaseToCrownCourt(caseId, defendantId);
         // when
-        final String response = getCasesForSearchCriteria("Kane+Junior");
+        addProsecutionCaseToCrownCourt(caseId, defendantId);
         // then
-        final JsonObject prosecutioncasesJsonObject = getJsonObject(response);
-        assertNotNull(prosecutioncasesJsonObject);
-        assertTrue(prosecutioncasesJsonObject.getJsonArray("searchResults").getJsonObject(0).getString("defendantName").contains(lastName));
+        verifyCasesForSearchCriteria("Kane+Junior", new Matcher[]{withJsonPath("$.searchResults[0].defendantName",containsString(lastName))});
     }
 
     @Test
     public void shouldGetProsecutionCaseByFirstAndMiddleName() throws Exception {
-        // given
+        // when
         addProsecutionCaseToCrownCourt(caseId, defendantId);
         final String firstAndMiddleName = "Harry+Jack";
-        // when
-        final String response = getCasesForSearchCriteria(firstAndMiddleName);
         // then
-        final JsonObject prosecutioncasesJsonObject = getJsonObject(response);
-        assertNotNull(prosecutioncasesJsonObject);
-        assertTrue(prosecutioncasesJsonObject.getJsonArray("searchResults").getJsonObject(0).getString("defendantName").contains(middleName));
+        verifyCasesForSearchCriteria(firstAndMiddleName, new Matcher[]{withJsonPath("$.searchResults[0].defendantName",containsString(middleName))});
     }
 
     @Test
     public void shouldGetProsecutionCaseByFirstNameAfterDefendantUpdate() throws Exception {
         // given
         addProsecutionCaseToCrownCourt(caseId, defendantId);
-        String response = getCasesForSearchCriteria(firstName);
-        JsonObject prosecutioncasesJsonObject = getJsonObject(response);
+        verifyCasesForSearchCriteria(firstName, new Matcher[]{withJsonPath("$.searchResults[0].defendantName",containsString(firstName))});
         final String updatedFirstName = "updatedName";
         // when
         helper.updateDefendant();
@@ -132,94 +108,63 @@ public class SearchCasesIT {
         // then
         helper.verifyInActiveMQ();
         helper.verifyInMessagingQueueForDefendentChanged();
-        response = getCasesForSearchCriteria(updatedFirstName);
-        prosecutioncasesJsonObject = getJsonObject(response);
-        assertNotNull(prosecutioncasesJsonObject);
-        assertTrue(prosecutioncasesJsonObject.getJsonArray("searchResults").getJsonObject(0).getString("defendantName").contains(updatedFirstName));
+        verifyCasesForSearchCriteria(updatedFirstName, new Matcher[]{withJsonPath("$.searchResults[0].defendantName",containsString(updatedFirstName))});
     }
 
     @Test
     public void shouldGetProsecutionCaseByReference() throws Exception {
-        // given
-        addProsecutionCaseWithUrn(caseId, defendantId,"URN12345");
         // when
-        final String response = getCasesForSearchCriteria("URN12345");
+        addProsecutionCaseWithUrn(caseId, defendantId,"URN12345");
         // then
-        final JsonObject prosecutioncasesJsonObject = getJsonObject(response);
-        assertNotNull(prosecutioncasesJsonObject);
-        assertEquals("URN12345", prosecutioncasesJsonObject.getJsonArray("searchResults").getJsonObject(0).getString("reference"));
+        verifyCasesForSearchCriteria("URN12345", new Matcher[]{withJsonPath("$.searchResults[0].reference",equalTo("URN12345"))});
     }
 
     @Test
     public void shouldGetProsecutionCaseByDobDDMMMyy() throws Exception {
-        // given
-        addProsecutionCaseToCrownCourt(caseId, defendantId);
         // when
-        final String response = getCasesForSearchCriteria("1-Jan-2010");
+        addProsecutionCaseToCrownCourt(caseId, defendantId);
         // then
-        final JsonObject prosecutioncasesJsonObject = getJsonObject(response);
-        assertNotNull(prosecutioncasesJsonObject);
-        assertEquals("2010-01-01", prosecutioncasesJsonObject.getJsonArray("searchResults").getJsonObject(0).getString("dob"));
+        verifyCasesForSearchCriteria("1-Jan-2010", new Matcher[]{withJsonPath("$.searchResults[0].dob",equalTo("2010-01-01"))});
     }
 
     @Test
     public void shouldGetProsecutionCaseByDobyyyyMMDD() throws Exception {
-        // given
-        addProsecutionCaseToCrownCourt(caseId, defendantId);
         // when
-        final String response = getCasesForSearchCriteria("01/01/10");
+        addProsecutionCaseToCrownCourt(caseId, defendantId);
         // then
-        final JsonObject prosecutioncasesJsonObject = getJsonObject(response);
-        assertNotNull(prosecutioncasesJsonObject);
-        assertEquals("2010-01-01", prosecutioncasesJsonObject.getJsonArray("searchResults").getJsonObject(0).getString("dob"));
+        verifyCasesForSearchCriteria("01/01/10", new Matcher[]{withJsonPath("$.searchResults[0].dob",equalTo("2010-01-01"))});
     }
 
     @Test
     public void shouldGetProsecutionCaseByDobDMMyy() throws Exception {
-        // given
-        addProsecutionCaseToCrownCourt(caseId, defendantId);
         // when
-        final String response = getCasesForSearchCriteria("1/01/10");
+        addProsecutionCaseToCrownCourt(caseId, defendantId);
         // then
-        final JsonObject prosecutioncasesJsonObject = getJsonObject(response);
-        assertNotNull(prosecutioncasesJsonObject);
-        assertEquals("2010-01-01", prosecutioncasesJsonObject.getJsonArray("searchResults").getJsonObject(0).getString("dob"));
+        verifyCasesForSearchCriteria("1/01/10", new Matcher[]{withJsonPath("$.searchResults[0].dob",equalTo("2010-01-01"))});
     }
 
     @Test
     public void shouldGetProsecutionCaseByDOBDdMMyyyy() throws Exception {
-        // given
-        addProsecutionCaseToCrownCourt(caseId, defendantId);
         // when
-        final String response = getCasesForSearchCriteria("01/01/2010");
+        addProsecutionCaseToCrownCourt(caseId, defendantId);
         // then
-        final JsonObject prosecutioncasesJsonObject = getJsonObject(response);
-        assertNotNull(prosecutioncasesJsonObject);
-        assertEquals("2010-01-01", prosecutioncasesJsonObject.getJsonArray("searchResults").getJsonObject(0).getString("dob"));
+        verifyCasesForSearchCriteria("01/01/2010", new Matcher[]{withJsonPath("$.searchResults[0].dob",equalTo("2010-01-01"))});
     }
 
     @Test
     public void shouldGetProsecutionCaseByDOB_d_MM_yyyy() throws Exception {
-        // given
-        addProsecutionCaseToCrownCourt(caseId, defendantId);
         // when
-        final String response = getCasesForSearchCriteria("1-01-2010");
+        addProsecutionCaseToCrownCourt(caseId, defendantId);
         // then
-        final JsonObject prosecutioncasesJsonObject = getJsonObject(response);
-        assertNotNull(prosecutioncasesJsonObject);
-        assertEquals("2010-01-01", prosecutioncasesJsonObject.getJsonArray("searchResults").getJsonObject(0).getString("dob"));
+        verifyCasesForSearchCriteria("1-01-2010", new Matcher[]{withJsonPath("$.searchResults[0].dob",equalTo("2010-01-01"))});
     }
 
     @Test
     public void shouldGetProsecutionCaseByDOB_dd_MM_yy() throws Exception {
-        // given
-        addProsecutionCaseToCrownCourt(caseId, defendantId);
         // when
-        final String response = getCasesForSearchCriteria("01-01-10");
+        addProsecutionCaseToCrownCourt(caseId, defendantId);
         // then
-        final JsonObject prosecutioncasesJsonObject = getJsonObject(response);
-        assertNotNull(prosecutioncasesJsonObject);
-        assertEquals("2010-01-01", prosecutioncasesJsonObject.getJsonArray("searchResults").getJsonObject(0).getString("dob"));
+        verifyCasesForSearchCriteria("01-01-10", new Matcher[]{withJsonPath("$.searchResults[0].dob",equalTo("2010-01-01"))});
     }
 }
 
