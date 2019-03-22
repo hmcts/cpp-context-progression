922a42757b2cc39c3ccdc9cf5f36131b4ed859d4
amckenzie <allan.mckenzie@agilesphere.co.uk>
2019-03-22 15:40:51 +0000

    Update to event-store 1.1.8 to allow for replay tool fixes
    Change-Id: Ie5dc23b662f8e21cdfb05b3b204f34cf875eb2f4

--- runIntegrationTests.sh
+++ runIntegrationTests.sh
@@ -7,9 +7,9 @@
 ${VAGRANT_DIR:?"Please export VAGRANT_DIR environment variable to point at atcm-vagrant"}
 declare WILDFLY_DEPLOYMENT_DIR="${VAGRANT_DIR}/deployments"
 declare CONTEXT_NAME=progression
-declare EVENT_LOG_VERSION=1.1.4
-declare EVENT_BUFFER_VERSION=1.1.4
-declare FILE_SERVICE_VERSION=1.14.0
+declare EVENT_LOG_VERSION=1.1.8
+declare EVENT_BUFFER_VERSION=1.1.8
+declare FILE_SERVICE_VERSION=1.17.1
 
 
 #fail script on error
