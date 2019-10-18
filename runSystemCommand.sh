6900bdfeff5ae95fe5ca666eb8950cb941eceb18
Shaun Francis <shaun@mapingo.co.uk>
2019-10-18 16:05:32 +0100

    Update service parent pom to 9.3.1
    Change-Id: I6cb76e3c9686b7b41201b2cd087f4e113b72a4c6

--- runSystemCommand.sh
+++ runSystemCommand.sh
@@ -17,7 +17,7 @@
 #
 ##################################################################################################
 
-FRAMEWORK_JMX_COMMAND_CLIENT_VERSION=2.2.1
+FRAMEWORK_JMX_COMMAND_CLIENT_VERSION=2.2.2
 CONTEXT_NAME="material"
 USER_NAME="admin"
 PASSWORD="admin"
