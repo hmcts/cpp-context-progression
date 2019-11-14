4f186a765d4fc176f381efb2914a2448e8ec5915
Shaun Francis <shaun.francis@hmcts.net>
2019-11-14 14:59:27 +0000

    Revert "Update framework to 6.4.0"
    This reverts commit 1ced64ac956372cf2dbef207b63acbc165bf4bd0.
    
    Reason for revert: Merged unintentionally
    
    Change-Id: I8f8bcb95e4dfbabbe03082473f5fe8f5bf5692b8

--- runSystemCommand.sh
+++ runSystemCommand.sh
@@ -17,8 +17,8 @@
 #
 ##################################################################################################
 
-FRAMEWORK_JMX_COMMAND_CLIENT_VERSION=2.4.0
-CONTEXT_NAME="progression"
+FRAMEWORK_JMX_COMMAND_CLIENT_VERSION=2.2.3
+CONTEXT_NAME="material"
 USER_NAME="admin"
 PASSWORD="admin"
 
