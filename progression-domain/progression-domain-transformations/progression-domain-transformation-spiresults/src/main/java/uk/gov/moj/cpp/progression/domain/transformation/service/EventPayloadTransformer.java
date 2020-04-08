52bf2ef845d18da1d5d79fe21690e0f1df7f337c
Mahesh Subramanian <mahesh.subramanian1@hmcts.net>
2020-04-08 22:50:24 +0100

    Fix for misspelt prompt label
    Change-Id: I875893f2984995c8cff76dba4d0caa3905c326c5

--- progression-domain/progression-domain-transformations/progression-domain-transformation-spiresults/src/main/java/uk/gov/moj/cpp/progression/domain/transformation/service/EventPayloadTransformer.java
+++ progression-domain/progression-domain-transformations/progression-domain-transformation-spiresults/src/main/java/uk/gov/moj/cpp/progression/domain/transformation/service/EventPayloadTransformer.java
@@ -13,6 +13,7 @@ import uk.gov.moj.cpp.progression.domain.transformation.helper.JsonPathHelper;
 import java.io.StringReader;
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
+import java.util.List;
 import java.util.Map;
 import java.util.Optional;
 import java.util.Set;
@@ -20,6 +21,7 @@ import java.util.Set;
 import javax.json.JsonReader;
 
 import com.google.common.annotations.VisibleForTesting;
+import com.google.common.collect.Lists;
 import com.google.common.collect.Maps;
 import com.google.gson.Gson;
 import com.google.gson.JsonElement;
@@ -34,6 +36,8 @@ public class EventPayloadTransformer {
     public static final String JUDICIAL_RESULTS_ATTRIBUTE = "judicialResults";
     public static final String JUDICIAL_RESULT_PROMPTS_ATTRIBUTE = "judicialResultPrompts";
     public static final DateTimeFormatter DATE_TIME_FORMATTER = DateTimeFormatter.ofPattern("yyyy-MM-dd");
+    public static final String DATE_OF_HEARING_CORRECT_SPELLING = "Date of hearing";
+    public static final String DATE_OF_HEARING_INCORRECT_SPELLING = "Date of hearig";
     public static final String LABEL_ATTRIBUTE_NAME = "label";
     public static final String ATTRIBUTE_VALUE_YES = "Y";
 
@@ -160,7 +164,7 @@ public class EventPayloadTransformer {
                     final boolean availableForCourtExtractAttributePresent = judicialResultPrompt.has(isAvailableForCourtExtractAttribute);
                     final boolean isAvailableForCourtExtract = availableForCourtExtractAttributePresent && judicialResultPrompt.get(isAvailableForCourtExtractAttribute).getAsBoolean();
                     final String promptLabel = judicialResultPrompt.get(LABEL_ATTRIBUTE_NAME).getAsString();
-                    final Optional<Prompt> optionalPrompt = resultDefinition.getPrompts().stream().filter(p -> p.getLabel().equals(promptLabel)).findFirst();
+                    final Optional<Prompt> optionalPrompt = getMatchingPrompt(resultDefinition, promptLabel);
                     if (!optionalPrompt.isPresent()) {
                         throw new TransformationException(format("No matching prompt found for label '%s' for date '%s' in result definition with ID '%s'", promptLabel, eventPublishedDate, resultDefinition.getId()));
                     }
@@ -172,6 +176,20 @@ public class EventPayloadTransformer {
                 }
             });
         }
+    }
+
+    private Optional<Prompt> getMatchingPrompt(final ResultDefinition resultDefinition, final String promptLabel) {
+        return resultDefinition.getPrompts().stream().filter(p -> {
+            final boolean promptFound = p.getLabel().equals(promptLabel);
+
+            if (promptFound) {
+                return true;
+            }
+
+            // this is a workaround for mispelt prompt label
+            final List<String> possiblePromptLabelOptions = Lists.newArrayList(DATE_OF_HEARING_CORRECT_SPELLING, DATE_OF_HEARING_INCORRECT_SPELLING);
+            return possiblePromptLabelOptions.stream().anyMatch(promptLabel::equalsIgnoreCase);
+        }).findFirst();
     }
 
     private Optional<String> getHearingIdFromEvent(final String eventPayload, final String eventName) {
