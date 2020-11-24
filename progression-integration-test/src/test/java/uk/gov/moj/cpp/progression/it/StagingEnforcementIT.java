99cd5a56d8def5768d4ec140375728e21eace072
mohammed.gazi <mohammed.gazi@hmcts.net>
2020-11-24 09:56:23 +0000

    DD-4924 Duplicate Notice of Compensation and Notice of Financial Penalty NOWs
    Change-Id: I67f8a9a3da69e0e80378861fc0f5fef7d8cbdc09

--- progression-integration-test/src/test/java/uk/gov/moj/cpp/progression/it/StagingEnforcementIT.java
+++ progression-integration-test/src/test/java/uk/gov/moj/cpp/progression/it/StagingEnforcementIT.java
@@ -1,16 +1,5 @@
 package uk.gov.moj.cpp.progression.it;
 
-import org.junit.After;
-import org.junit.Before;
-import org.junit.Test;
-import uk.gov.moj.cpp.progression.AbstractIT;
-import uk.gov.moj.cpp.progression.helper.NowsRequestHelper;
-
-import javax.jms.JMSException;
-import javax.jms.MessageProducer;
-import javax.json.JsonObject;
-import java.util.UUID;
-
 import static java.util.UUID.randomUUID;
 import static javax.json.Json.createObjectBuilder;
 import static uk.gov.justice.services.test.utils.core.messaging.MetadataBuilderFactory.metadataOf;
@@ -18,6 +7,19 @@ import static uk.gov.moj.cpp.progression.helper.Cleaner.closeSilently;
 import static uk.gov.moj.cpp.progression.helper.QueueUtil.publicEvents;
 import static uk.gov.moj.cpp.progression.helper.QueueUtil.sendMessage;
 import static uk.gov.moj.cpp.progression.util.FileUtil.getPayload;
+
+import uk.gov.moj.cpp.progression.AbstractIT;
+import uk.gov.moj.cpp.progression.helper.NowsRequestHelper;
+
+import java.util.UUID;
+
+import javax.jms.JMSException;
+import javax.jms.MessageProducer;
+import javax.json.JsonObject;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
 
 public class StagingEnforcementIT extends AbstractIT {
 
@@ -36,28 +38,63 @@ public class StagingEnforcementIT extends AbstractIT {
     public void shouldReceiveAcknowledgement() {
         final String requestId = UUID.randomUUID().toString();
         final String accountNumber = "AER123451";
-        sendPublicEvent(requestId, accountNumber);
-        nowsRequestHelper.verifyAccountNumberAddedToRequest(accountNumber, requestId);
+        makeNowsRequest(requestId);
+        sendSuccessAcknowledgement(requestId, accountNumber);
     }
 
     @Test
     public void shouldNotReceiveAcknowledgementWhenRequestDoubled() {
         final String requestId = UUID.randomUUID().toString();
         final String accountNumber = "AER123451";
-
-        sendPublicEvent(requestId, accountNumber);
+        makeNowsRequest(requestId);
+        sendSuccessAcknowledgement(requestId, accountNumber);
         nowsRequestHelper.verifyAccountNumberAddedToRequest(accountNumber, requestId);
-
-        sendPublicEvent(requestId, accountNumber);
+        sendSuccessAcknowledgement(requestId, accountNumber);
         nowsRequestHelper.verifyAccountNumberIgnoredToRequest(accountNumber, requestId);
     }
 
     @Test
     public void shouldReceiveErrorAcknowledgement() {
         final String requestId = UUID.randomUUID().toString();
+        makeNowsRequest(requestId);
+        sendErrorAcknowledgementAndVerify(requestId);
+    }
+
+    @After
+    public void tearDown() throws JMSException {
+        producer.close();
+        closeSilently(nowsRequestHelper);
+    }
+
+    private void makeNowsRequest(final String requestId) {
         final String payload = this.prepareAddNowDocumentRequestPayload(requestId);
         nowsRequestHelper = new NowsRequestHelper();
         nowsRequestHelper.makeNowsRequest(requestId, payload);
+    }
+
+    private String prepareAddNowDocumentRequestPayload(final String requestId) {
+        String body = getPayload("enforcement/progression.add-now-document-request.json");
+        body = body.replaceAll("%HEARING_ID%", UUID.randomUUID().toString())
+                .replaceAll("%MATERIAL_ID%", UUID.randomUUID().toString())
+                .replaceAll("%REQUEST_ID%", requestId)
+                .replaceAll("%DEFENDANT_ID%", UUID.randomUUID().toString());
+        return body;
+    }
+
+    private void sendSuccessAcknowledgement(final String requestId, final String accountNumber ){
+        final JsonObject stagingEnforcementAckPayload = createObjectBuilder().add("originator", "courts")
+                .add("requestId", requestId)
+                .add("exportStatus", "ENFORCEMENT_ACKNOWLEDGED")
+                .add("updated", "2019-12-01T10:00:00Z")
+                .add("acknowledgement", createObjectBuilder().add("accountNumber", accountNumber).build())
+                .build();
+
+        sendMessage(producer, PUBLIC_EVENT_STAGINGENFORCEMENT_ENFORCE_FINANCIAL_IMPOSITION_ACKNOWLEDGEMENT, stagingEnforcementAckPayload,
+                metadataOf(randomUUID(), PUBLIC_EVENT_STAGINGENFORCEMENT_ENFORCE_FINANCIAL_IMPOSITION_ACKNOWLEDGEMENT)
+                        .withUserId(USER_ID_VALUE_AS_ADMIN.toString()).build());
+    }
+
+    private void sendErrorAcknowledgementAndVerify(final String requestId) {
         final String errorCode = "ERR12";
         final String errorMessage = "Post code is invalid";
         final JsonObject stagingEnforcementAckPayload = createObjectBuilder().add("originator", "courts")
@@ -72,36 +109,5 @@ public class StagingEnforcementIT extends AbstractIT {
                         .withUserId(USER_ID_VALUE_AS_ADMIN.toString()).build());
 
         nowsRequestHelper.verifyErrorEventRaised(errorCode, errorMessage);
-    }
-
-    private String prepareAddNowDocumentRequestPayload(final String requestId) {
-        String body = getPayload("enforcement/progression.add-now-document-request.json");
-        body = body.replaceAll("%HEARING_ID%", UUID.randomUUID().toString())
-                .replaceAll("%MATERIAL_ID%", UUID.randomUUID().toString())
-                .replaceAll("%REQUEST_ID%", requestId)
-                .replaceAll("%DEFENDANT_ID%", UUID.randomUUID().toString());
-        return body;
-    }
-
-    @After
-    public void tearDown() throws JMSException {
-        producer.close();
-        closeSilently(nowsRequestHelper);
-    }
-
-    private void sendPublicEvent(final String requestId, final String accountNumber ){
-        final String payload = this.prepareAddNowDocumentRequestPayload(requestId);
-        nowsRequestHelper = new NowsRequestHelper();
-        nowsRequestHelper.makeNowsRequest(requestId, payload);
-        final JsonObject stagingEnforcementAckPayload = createObjectBuilder().add("originator", "courts")
-                .add("requestId", requestId)
-                .add("exportStatus", "ENFORCEMENT_ACKNOWLEDGED")
-                .add("updated", "2019-12-01T10:00:00Z")
-                .add("acknowledgement", createObjectBuilder().add("accountNumber", accountNumber).build())
-                .build();
-
-        sendMessage(producer, PUBLIC_EVENT_STAGINGENFORCEMENT_ENFORCE_FINANCIAL_IMPOSITION_ACKNOWLEDGEMENT, stagingEnforcementAckPayload,
-                metadataOf(randomUUID(), PUBLIC_EVENT_STAGINGENFORCEMENT_ENFORCE_FINANCIAL_IMPOSITION_ACKNOWLEDGEMENT)
-                        .withUserId(USER_ID_VALUE_AS_ADMIN.toString()).build());
     }
 }
