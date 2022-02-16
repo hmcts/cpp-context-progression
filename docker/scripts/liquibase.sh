fe8c8d011b3000314f2ea35bf8814c16c51d669d
varunvijayagopal <varun.vijayagopal@hmcts.net>
2022-02-16 16:22:01 +0000

    docker poc
    Change-Id: I1dca4715f47fafeee26f5834af0eab15922fc8ab

---
+++ docker/scripts/liquibase.sh
@@ -0,0 +1,53 @@
+echo "Running Liquibase"
+dbServerName=$1
+dbUserName=$2
+dbPassword=$3
+java -jar event-repository-liquibase.jar --url=jdbc:postgresql://${dbServerName}:5432/progressioneventstore?sslmode=require --username=${dbUserName} --password=${dbPassword} --logLevel=info update
+if [ $? -ne 0 ]
+then
+    exit 1
+else
+    echo success!
+fi
+java -jar aggregate-snapshot-repository-liquibase.jar --url=jdbc:postgresql://${dbServerName}:5432/progressioneventstore?sslmode=require --username=${dbUserName} --password=${dbPassword} --logLevel=info update
+if [ $? -ne 0 ]
+then
+    exit 1
+else
+    echo success!
+fi
+java -jar event-buffer-liquibase.jar --url=jdbc:postgresql://${dbServerName}:5432/progressionviewstore?sslmode=require --username=${dbUserName} --password=${dbPassword} --logLevel=info update
+if [ $? -ne 0 ]
+then
+    exit 1
+else
+    echo success!
+fi
+java -jar event-tracking-liquibase.jar --url=jdbc:postgresql://${dbServerName}:5432/progressionviewstore?sslmode=require --username=${dbUserName} --password=${dbPassword} --logLevel=info update
+if [ $? -ne 0 ]
+then
+    exit 1
+else
+    echo success!
+fi
+java -jar progression-viewstore-liquibase.jar --url=jdbc:postgresql://${dbServerName}:5432/progressionviewstore?sslmode=require --username=${dbUserName} --password=${dbPassword} --logLevel=info update
+if [ $? -ne 0 ]
+then
+    exit 1
+else
+    echo success!
+fi
+java -jar framework-system-liquibase.jar --url=jdbc:postgresql://${dbServerName}:5432/progressionsystem?sslmode=require --username=${dbUserName} --password=${dbPassword} --logLevel=info update
+if [ $? -ne 0 ]
+then
+    exit 1
+else
+    echo success!
+fi
+java -jar activiti-liquibase.jar --url=jdbc:postgresql://${dbServerName}:5432/progressionactiviti?sslmode=require --username=${dbUserName} --password=${dbPassword} --logLevel=info update
+if [ $? -ne 0 ]
+then
+    exit 1
+else
+    echo success!
+fi
