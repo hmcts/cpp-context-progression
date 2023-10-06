a4c80dbf68d2f156fac5cf8d0e4fe486691ab267
yashpalkumar.negi <yashpalkumar.negi@hmcts.net>
2023-10-06 14:47:14 +0100

    DD-28582 -  added Magistrates role in the drool rule list
    Change-Id: Ie532fcb21ad367eae118cd49513a8636e631a356

---
+++ progression-query/progression-query-api/src/test/java/rules/ProgressionCommandRuleExecutorTest.java
@@ -0,0 +1,73 @@
+package rules;
+
+import com.google.common.collect.ImmutableMap;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.kie.api.runtime.ExecutionResults;
+import org.mockito.Mock;
+import org.mockito.runners.MockitoJUnitRunner;
+import uk.gov.moj.cpp.accesscontrol.common.providers.UserAndGroupProvider;
+import uk.gov.moj.cpp.accesscontrol.drools.Action;
+import uk.gov.moj.cpp.accesscontrol.refdata.providers.RbacProvider;
+import uk.gov.moj.cpp.accesscontrol.test.utils.BaseDroolsAccessControlTest;
+
+import java.util.Arrays;
+import java.util.Map;
+
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+@RunWith(MockitoJUnitRunner.class)
+public class ProgressionCommandRuleExecutorTest extends BaseDroolsAccessControlTest {
+
+    protected Action action;
+    @Mock
+    protected UserAndGroupProvider userAndGroupProvider;
+    @Mock
+    protected RbacProvider rbacProvider;
+
+    @Override
+    protected Map<Class, Object> getProviderMocks() {
+        return ImmutableMap.<Class, Object>builder()
+                .put(RbacProvider.class, rbacProvider)
+                .put(UserAndGroupProvider.class, userAndGroupProvider)
+                .build();
+    }
+
+    @Test
+    public void whenUserIsAMemberOfAllowedUserGroups() {
+        Arrays.stream(ProgressionQueryRules.values()).forEach(ruleTest -> {
+            action = createActionFor(ruleTest.actionName);
+            when(userAndGroupProvider.isMemberOfAnyOfTheSuppliedGroups(action, ruleTest.allowedUserGroups)).thenReturn(true);
+            final ExecutionResults executionResults = executeRulesWith(action);
+            assertSuccessfulOutcome(executionResults);
+            verify(userAndGroupProvider).isMemberOfAnyOfTheSuppliedGroups(action, ruleTest.allowedUserGroups);
+        });
+    }
+
+    @Test
+    public void whenUserIsNotAMemberOfAllowedUserGroups_thenFailUpload() throws Exception {
+        Arrays.stream(ProgressionQueryRules.values()).forEach(ruleTest -> {
+            action = createActionFor(ruleTest.actionName);
+            when(userAndGroupProvider.isMemberOfAnyOfTheSuppliedGroups(action, ruleTest.allowedUserGroups)).thenReturn(false);
+            final ExecutionResults executionResults = executeRulesWith(action);
+            assertFailureOutcome(executionResults);
+            verify(userAndGroupProvider).isMemberOfAnyOfTheSuppliedGroups(action, ruleTest.allowedUserGroups);
+        });
+    }
+
+    public enum ProgressionQueryRules {
+
+        ApplicationHearing("progression.query.applicationhearings", "Judiciary", "Listing Officers", "Legal Advisers", "Court Associate", "Court Clerks", "NCES", "CPS", "Probation Admin", "Youth Offending Service Admin",
+                "Court Administrators", "Crown Court Admin", "Judge", "Victims & Witness Care Admin", "Police Admin", "Recorders", "DJMC", "Deputies", "Magistrates");
+
+        private final String actionName;
+        private final String[] allowedUserGroups;
+
+        ProgressionQueryRules(final String actionName, final String... allowedUserGroups) {
+            this.actionName = actionName;
+            this.allowedUserGroups = allowedUserGroups;
+        }
+    }
+
+}
