66369e185b587985ce5da0d4f4e699f7e00e4e69
Sajan Chandran <sajan.chandran@hmcts.net>
2023-10-13 18:09:35 +0100

    SNI-4588 HMPPS API not providing listing information
    Change-Id: I1ba43bd523d27622d80fcbdcfab7218f518bc4e2

--- progression-event/progression-event-listener/src/main/java/uk/gov/moj/cpp/prosecutioncase/event/listener/DefendantsAddedToCourtProceedingsListener.java
+++ progression-event/progression-event-listener/src/main/java/uk/gov/moj/cpp/prosecutioncase/event/listener/DefendantsAddedToCourtProceedingsListener.java
@@ -2,16 +2,20 @@ package uk.gov.moj.cpp.prosecutioncase.event.listener;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import uk.gov.justice.core.courts.Hearing;
 import uk.gov.justice.core.courts.Offence;
 import uk.gov.justice.core.courts.ProsecutionCase;
 import uk.gov.justice.core.courts.DefendantsAddedToCourtProceedings;
+import uk.gov.justice.cpp.progression.events.NewDefendantAddedToHearing;
 import uk.gov.justice.services.common.converter.JsonObjectToObjectConverter;
 import uk.gov.justice.services.common.converter.ObjectToJsonObjectConverter;
 import uk.gov.justice.services.common.converter.StringToJsonObjectConverter;
 import uk.gov.justice.services.core.annotation.Handles;
 import uk.gov.justice.services.core.annotation.ServiceComponent;
 import uk.gov.justice.services.messaging.JsonEnvelope;
+import uk.gov.moj.cpp.prosecutioncase.persistence.entity.HearingEntity;
 import uk.gov.moj.cpp.prosecutioncase.persistence.entity.ProsecutionCaseEntity;
+import uk.gov.moj.cpp.prosecutioncase.persistence.repository.HearingRepository;
 import uk.gov.moj.cpp.prosecutioncase.persistence.repository.ProsecutionCaseRepository;
 
 import javax.inject.Inject;
@@ -41,6 +45,9 @@ public class DefendantsAddedToCourtProceedingsListener {
     @Inject
     private ProsecutionCaseRepository repository;
 
+    @Inject
+    private HearingRepository hearingRepository;
+
     private static final Logger LOGGER = LoggerFactory.getLogger(DefendantsAddedToCourtProceedingsListener.class);
 
     @Handles("progression.event.defendants-added-to-court-proceedings")
@@ -60,6 +67,21 @@ public class DefendantsAddedToCourtProceedingsListener {
             prosecutionCaseEntity.setPayload(objectToJsonObjectConverter.convert(prosecutionCase).toString());
             repository.save(prosecutionCaseEntity);
         }
+    }
+
+    @Handles("progression.event.new-defendant-added-to-hearing")
+    public void addNewDefendantToHearing(final JsonEnvelope event) {
+        if(LOGGER.isDebugEnabled()) {
+            LOGGER.debug("received in event listener progression.event.new-defendant-added-to-hearing {} ", event.toObfuscatedDebugString());
+        }
+        final NewDefendantAddedToHearing newDefendantAddedToHearing = jsonObjectConverter.convert(event.payloadAsJsonObject(), NewDefendantAddedToHearing.class);
+        final HearingEntity hearingEntity = hearingRepository.findBy(newDefendantAddedToHearing.getHearingId());
+        final JsonObject hearingJson = stringToJsonObjectConverter.convert(hearingEntity.getPayload());
+        final Hearing hearing = jsonObjectConverter.convert(hearingJson, Hearing.class);
+        hearing.getProsecutionCases().stream().filter(prosecutionCase -> prosecutionCase.getId().equals(newDefendantAddedToHearing.getProsecutionCaseId()))
+                .forEach(prosecutionCase -> prosecutionCase.getDefendants().addAll(newDefendantAddedToHearing.getDefendants()));
+        hearingEntity.setPayload(objectToJsonObjectConverter.convert(hearing).toString());
+        hearingRepository.save(hearingEntity);
     }
 
     private void filterDuplicateOffencesById(final List<Offence> offences) {
