46c1608f66dee0e2e75389f070ecc703103f437a
gunnertwin <daniel@alvestechnology.io>
2024-02-22 15:59:02 +0000

    Update liquibase script to use dynamic dbPort
    Change-Id: I4f69f60c941904e920ef6dc6d1d568e89803fd16

--- docker/scripts/liquibase.sh
+++ docker/scripts/liquibase.sh
@@ -2,49 +2,50 @@ echo "Running Liquibase"
 dbServerName=$1
 dbUserName=$2
 dbPassword=$3
-java -jar event-repository-liquibase.jar --url=jdbc:postgresql://${dbServerName}:5432/progressioneventstore?sslmode=require --username=${dbUserName} --password=${dbPassword} --logLevel=info update
+dbPort=${5:-5432}
+java -jar event-repository-liquibase.jar --url=jdbc:postgresql://${dbServerName}:${dbPort}/progressioneventstore?sslmode=require --username=${dbUserName} --password=${dbPassword} --logLevel=info update
 if [ $? -ne 0 ]
 then
     exit 1
 else
     echo success!
 fi
-java -jar aggregate-snapshot-repository-liquibase.jar --url=jdbc:postgresql://${dbServerName}:5432/progressioneventstore?sslmode=require --username=${dbUserName} --password=${dbPassword} --logLevel=info update
+java -jar aggregate-snapshot-repository-liquibase.jar --url=jdbc:postgresql://${dbServerName}:${dbPort}/progressioneventstore?sslmode=require --username=${dbUserName} --password=${dbPassword} --logLevel=info update
 if [ $? -ne 0 ]
 then
     exit 1
 else
     echo success!
 fi
-java -jar event-buffer-liquibase.jar --url=jdbc:postgresql://${dbServerName}:5432/progressionviewstore?sslmode=require --username=${dbUserName} --password=${dbPassword} --logLevel=info update
+java -jar event-buffer-liquibase.jar --url=jdbc:postgresql://${dbServerName}:${dbPort}/progressionviewstore?sslmode=require --username=${dbUserName} --password=${dbPassword} --logLevel=info update
 if [ $? -ne 0 ]
 then
     exit 1
 else
     echo success!
 fi
-java -jar event-tracking-liquibase.jar --url=jdbc:postgresql://${dbServerName}:5432/progressionviewstore?sslmode=require --username=${dbUserName} --password=${dbPassword} --logLevel=info update
+java -jar event-tracking-liquibase.jar --url=jdbc:postgresql://${dbServerName}:${dbPort}/progressionviewstore?sslmode=require --username=${dbUserName} --password=${dbPassword} --logLevel=info update
 if [ $? -ne 0 ]
 then
     exit 1
 else
     echo success!
 fi
-java -jar progression-viewstore-liquibase.jar --url=jdbc:postgresql://${dbServerName}:5432/progressionviewstore?sslmode=require --username=${dbUserName} --password=${dbPassword} --logLevel=info update
+java -jar progression-viewstore-liquibase.jar --url=jdbc:postgresql://${dbServerName}:${dbPort}/progressionviewstore?sslmode=require --username=${dbUserName} --password=${dbPassword} --logLevel=info update
 if [ $? -ne 0 ]
 then
     exit 1
 else
     echo success!
 fi
-java -jar framework-system-liquibase.jar --url=jdbc:postgresql://${dbServerName}:5432/progressionsystem?sslmode=require --username=${dbUserName} --password=${dbPassword} --logLevel=info update
+java -jar framework-system-liquibase.jar --url=jdbc:postgresql://${dbServerName}:${dbPort}/progressionsystem?sslmode=require --username=${dbUserName} --password=${dbPassword} --logLevel=info update
 if [ $? -ne 0 ]
 then
     exit 1
 else
     echo success!
 fi
-java -jar activiti-liquibase.jar --url=jdbc:postgresql://${dbServerName}:5432/progressionactiviti?sslmode=require --username=${dbUserName} --password=${dbPassword} --logLevel=info update
+java -jar activiti-liquibase.jar --url=jdbc:postgresql://${dbServerName}:${dbPort}/progressionactiviti?sslmode=require --username=${dbUserName} --password=${dbPassword} --logLevel=info update
 if [ $? -ne 0 ]
 then
     exit 1
