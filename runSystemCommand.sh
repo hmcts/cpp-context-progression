75bf851eed676b7c4aaa70dd1da2303de8cbfd18
zeldal <zeldal.ozdemir1@hmcts.net>
2025-01-30 14:02:27 +0000

    PEG-1176 upgrade to framework 17.101.0 (#446)

--- runSystemCommand.sh
+++ runSystemCommand.sh
@@ -17,7 +17,7 @@
 #
 ##################################################################################################
 
-FRAMEWORK_VERSION=17.100.4
+FRAMEWORK_VERSION=17.101.6
 CONTEXT_NAME="progression"
 USER_NAME="admin"
 PASSWORD="admin"
