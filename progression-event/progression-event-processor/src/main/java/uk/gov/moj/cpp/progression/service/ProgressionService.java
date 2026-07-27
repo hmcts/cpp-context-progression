c208b08a8d885476dd27d28847fc77e63e9e7968
Arcadius Ahouansou <arcadius@menelic.com>
2026-07-27 18:33:06 +0100

    Revert "master-defendant with pncid"
    This reverts commit 79b36c823cabb0d4e7fbf1fc12ee220a5b695c71.

--- progression-event/progression-event-processor/src/main/java/uk/gov/moj/cpp/progression/service/ProgressionService.java
+++ progression-event/progression-event-processor/src/main/java/uk/gov/moj/cpp/progression/service/ProgressionService.java
@@ -1988,7 +1988,6 @@ public class ProgressionService {
             final List<CourtApplicationParty> respondents = new ArrayList<>();
             final MasterDefendant masterDefendant = MasterDefendant.masterDefendant()
                     .withPersonDefendant(defendant.getPersonDefendant())
-                    .withPncId(defendant.getPncId())
                     .withMasterDefendantId(defendant.getMasterDefendantId())
                     .withAssociatedPersons(defendant.getAssociatedPersons())
                     .withDefendantCase(singletonList(DefendantCase.defendantCase()
